b620e95aa0e71fc9d35fa95736429c02b298b8e4
pmg <dkzld159@naver.com>
2024-12-30 13:50:36 +0900

    공고 update

--- view-server/src/main/java/org/example/viewserver/controller/JobPostController.java
+++ view-server/src/main/java/org/example/viewserver/controller/JobPostController.java
@@ -1,11 +1,8 @@
 package org.example.viewserver.controller;
 
-
-
 import lombok.RequiredArgsConstructor;
 import org.example.viewserver.dto.GubunDto;
 import org.example.viewserver.service.GubunService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,9 +22,15 @@ public class JobPostController {
     public String regist(Model model) {
         // 구분 데이터를 가져옵니다.
         List<GubunDto> workTypeList = gubunService.getGubunList("WORK_TYPE");
-        List<GubunDto> edducationList = gubunService.getGubunList("EDUCATION");
+        List<GubunDto> jobRankList = gubunService.getGubunList("JOB_RANK");
+        List<GubunDto> workList = gubunService.getGubunList("WORK");
+        List<GubunDto> educationCodeList = gubunService.getGubunList("EDUCATION");
+        List<GubunDto> skilList = gubunService.getGubunList("SKILL");
+        model.addAttribute("workList", workList);
         model.addAttribute("workTypeList", workTypeList);
-
+        model.addAttribute("jobRankList", jobRankList);
+        model.addAttribute("educationCodeList", educationCodeList);
+        model.addAttribute("skilList", skilList);
         // regist-form.html 뷰를 반환합니다.
         return "job-post/regist-form";
     }
